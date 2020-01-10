d5638832c717cd2cc4fa8205f665fadeaf4ec873
zsigil <chickpox@freemail.hu>
2020-01-10 15:22:04 +0100

    footer setup

--- src/App.vue
+++ src/App.vue
@@ -9,7 +9,23 @@
     </v-content>
 
     <v-footer app>
-      <!-- -->
+      <v-col class="text-center">
+        <div class="footer__item">
+          coded by IldikoZsigmond
+        </div>
+        <div class="footer__item d-none d-md-inline-block">
+          |
+        </div>
+        <div class="footer__item">
+          {{new Date().getFullYear()}}
+        </div>
+        <div class="footer__item d-none d-md-inline-block">
+          |
+        </div>
+        <div class="footer__item">
+          Chingu Pre-Work Project
+        </div>
+      </v-col>
     </v-footer>
   </v-app>
 </template>
@@ -27,3 +43,12 @@ export default {
   }),
 };
 </script>
+
+<style lang="scss" scoped>
+.footer__item{
+  @media(min-width:960px){
+    display: inline-block;
+    padding: 0 3px;
+  }
+}
+</style>
